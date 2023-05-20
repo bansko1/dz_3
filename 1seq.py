ddc0c69191e2a9fba4a4b5a55b657a96eb27785f
bansko1 <133859016+bansko1@users.noreply.github.com>
2023-05-20 20:20:02 +0300

    Update 1seq.py
    add change

--- 1seq.py
+++ 1seq.py
@@ -4,4 +4,5 @@ for i in range(int(amount_of_elements)):
     sentense = f'Введите {i+1} элемент: '
     x = input(sentense)
     list_of_elements.append(int(x))
+list_of_elements.sort()
 print('Вывод:', list_of_elements)
